76928ded55d446cdd8a957785fed654c4dd1daff
sqrtofsaturn <sqrtofsaturn@gmail.com>
2016-11-23 08:21:22 -0700

    Add support for deleting OSX services

--- manage/util_darwin.go
+++ manage/util_darwin.go
@@ -5,12 +5,12 @@ import (
 	"path/filepath"
 )
 
-func connectorDirectory(homeDir, uuid string) string {
-	return filepath.Join(userServiceDirectory(homeDir), uuid)
-}
-
 func serviceName(uuid string) string {
 	return fmt.Sprintf("com.octoblu.%s", uuid)
+}
+
+func serviceDirectory() string {
+	return filepath.Join("/Library", "MeshbluConnectors")
 }
 
 func userServiceDirectory(homeDir string) string {
